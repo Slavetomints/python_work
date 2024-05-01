3ee5b2b1a3033f6519ac0572922618d00c82b208
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-05-01 16:27:39 -0500

    work on subclasses

---
+++ admin.py
@@ -0,0 +1,58 @@
+class Users:
+    """Have info on a user"""
+    def __init__ (self, first_name, last_name, username, password, gender):
+        """Initializes attributes to the user account."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+        self.gender = gender
+        self.login_attempts = 0
+
+    def describe_user(self):
+        """Describe the user"""
+        full_name = self.first_name + ' ' + self.last_name
+        print(f"{self.username}'s full name is {full_name}. \nTheir password is {self.password}.\nTheir gender is {self.gender}")
+
+    def greet_user(self):
+        """Give a personalized greeting to the user"""
+        print(f'Hello {self.username}')
+
+    def increment_login_attempts(self):
+        """Increment the number of login attempts"""
+        self.login_attempts += 1
+        return self.login_attempts
+    
+    def reset_login_attempts(self):
+        """Reset the number of login attempts"""
+        self.login_attempts = 0
+        return self.login_attempts
+
+    def user_status(self):
+        """Give a status update on the user"""
+        print(f'\nThe current user is {self.username}')
+        print(f'{self.username} has logged in {self.login_attempts} time(s)')
+
+class Admin(Users):
+    """Info specifically on admins"""
+
+    def __init__ (self, first_name, last_name, username, password, gender):
+        """
+        Initializes info from parent class
+        Initializes admin info
+        """
+        super().__init__ (first_name, last_name, username, password, gender)
+        self.privileges = [
+            'can make posts', 
+            'can see other users data', 
+            'can add/remove users'
+            ]
+    
+    def show_privileges(self):
+        """Display the admins privileges"""
+        print(f'{self.username} has the following privileges')
+        for privilege in self.privileges:
+            print(f'\n-{privilege}')
+
+daisy = Admin('daisy', 'hardwick', 'slavetomints', '12345678910', 'female')
+print(daisy.show_privileges())
---
+++ ice_cream_stand.py
@@ -0,0 +1,46 @@
+class Restaurant:
+    """Represents a restaurant"""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        """Initialize attributes to represent a restaurant"""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_of_customers = 0
+    
+    def describe_restaurant(self):
+        """Give info about a restaurant"""
+        print(f"The restaurant's name is {self.restaurant_name}.")
+        print(f"The restaurant serves {self.cuisine_type} food.")
+        print(f'The restaurant has served {self.number_of_customers} customer(s)')
+
+    def open_restaurant(self):
+        """Display the restaurant is open"""
+        print(f'{self.restaurant_name} is now open')
+
+    def set_number_served(self, customers):
+        """Allows you to set the number of customers served"""
+        self.number_of_customers = customers
+
+    def increment_number_served(self, increment):
+        """Allows you to increment the number of customers"""
+        self.number_of_customers += increment
+
+class IceCreamShop(Restaurant):
+    """Represents an ice cream shop"""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        """
+        Initialize attributes of parent class, then 
+        initialize attributes specific to an ice cream shop
+        """
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ['vanilla', 'chocolate', 'strawberry', 'blue moon']
+
+    def display_flavors(self):
+        """Displays the flavors an ice cream stand has"""
+        print(f'{self.restaurant_name} has these flavors:')
+        for flavor in self.flavors:
+            print(f'\n-{flavor.title()}')
+
+dairy_haus = IceCreamShop('Dairy Haus', 'Desert')
+print(dairy_haus.display_flavors())
