4eea78e1ea133614ae0def37ef0ae189c90c1509
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-05-03 08:46:20 -0500

    more classes work

---
+++ electric_car.py
@@ -0,0 +1,21 @@
+class Car:
+    """A simple attempt to represent a car"""
+
+    def __init__(self, make, model, year):
+        """Initialikze attributes to describe a car"""
+        self.make = make
+        self.model = model
+        self.year = year 
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's milage"""
+        print(f"This car has {self.odometer_reading} miles on it")
+
+    def update_odometer(self):
+        """"""
