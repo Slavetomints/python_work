9467ba99393fbca097a2589b5c5e938465b1fa05
Daisy Hardwick <161798204+Slavetomints@users.noreply.github.com>
2024-05-06 12:30:29 -0500

    started making dice

---
+++ dice.py
@@ -0,0 +1,11 @@
+from random import randint
+
+make die
+set sides equal to 6
+
+roll the die
+
+
+
+print(randint(1, 6))
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
